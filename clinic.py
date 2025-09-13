11766f2fe10951f1badb1c5c2f03d0f86a74d06d
Pankaj9897 <joshipankaj9897@gmail.com>
2025-09-13 15:31:10 +0530

    Update clinic.py
    for deloyment

--- clinic.py
+++ clinic.py
@@ -258,6 +258,4 @@ def api_predict():
             os.remove(tmp_path)
         except:
             pass
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+app.handler = app
